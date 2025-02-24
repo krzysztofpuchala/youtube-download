d5d08a99a97e3d33933f5a7c14ebd5efb5674052
Krzysztof Puchala <kp@pro-admin.pl>
2025-02-24 13:05:03 +0100

    update code, add playlist download

--- download.py
+++ download.py
@@ -7,15 +7,18 @@ def download_audio(youtube_url, output_path="downloads"):
     output_file = os.path.join(output_path, "%(title)s.%(ext)s")
 
     try:
-        # Pobieranie samego audio w formacie mp3
-#        subprocess.run(["yt-dlp", "-x", "--audio-format", "mp3", "-o", output_file, youtube_url], check=True)
-        subprocess.run(["yt-dlp", "-x", "--audio-format", "mp3", "--ffmpeg-location", "/opt/homebrew/bin/ffmpeg", "-o", output_file, youtube_url], check=True)
-        print(f"Pobrano plik do {output_path}")
+        # Pobieranie samego audio w formacie mp3 (obsługuje playlisty i pojedyncze filmy)
+        subprocess.run([
+            "yt-dlp", "-x", "--audio-format", "mp3",
+            "--ffmpeg-location", "/opt/homebrew/bin/ffmpeg",
+            "-o", output_file, youtube_url
+        ], check=True)
+        print(f"Pobrano plik(i) do {output_path}")
     except subprocess.CalledProcessError as e:
         print(f"Błąd: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Użycie: python script.py <URL YouTube>")
+        print("Użycie: python script.py <URL YouTube lub URL playlisty>")
     else:
         download_audio(sys.argv[1])
